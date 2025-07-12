0a96cbfb134bc1b6dae740a9d68361b1a895cc2c
danamdaya <danamdaya@gmail.com>
2025-07-12 18:19:14 +0000

    jdk

--- netlify/functions/vi_lunar_calendar/index.mjs
+++ netlify/functions/vi_lunar_calendar/index.mjs
@@ -14,8 +14,6 @@ export default async (req, context) => {
 
   const [user, pwd] = atob(authHeaders.split(' ')[1]).split(':');
 
-  console.log(Netlify.env.toObject());
-
   if (!Netlify.env.has(user) || pwd !== Netlify.env.get(user)) return new Response('403 Forbidden', {
     status: 403,
     headers: {
